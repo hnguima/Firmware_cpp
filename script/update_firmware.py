7493f0e66a31c2f34aa8eb4510e76feffe1b03f4
Henrique Guimarães <henrique.guimaraes@ati.com.br>
2023-03-06 12:55:01 -0300

    fix: updated firmware updater script to accept parameters

--- script/update_firmware.py
+++ script/update_firmware.py
@@ -1,9 +1,22 @@
 import requests
 import hashlib
+import argparse
+
+
+parser = argparse.ArgumentParser(
+                    prog = 'ESP - HTTP firmware updater',
+                    description = 'Script to update firmware on remote devices via HTTP. Set the device IP and firmware binary path to do the update.')
+
+
+parser.add_argument( '-b','--binary', metavar='', help='path to the binary to be uploaded')
+parser.add_argument('-i', '--ip', metavar='', help= 'IP address of the destination device')
+
+args = parser.parse_args()
+
+fileName = args.binary
+deviceIP = args.ip
 
 hash = hashlib.sha256()
-
-fileName = "./build/cp32wr.bin"
 
 with open(fileName, mode='rb') as file: # b is important -> binary
 
@@ -11,7 +24,7 @@ with open(fileName, mode='rb') as file: # b is important -> binary
   hash.update(fileContent)
   print(hash.hexdigest());
   
-  x = requests.put('http://192.168.0.24/update_firmware', data=fileContent,
+  x = requests.put(f'http://{deviceIP}/update_firmware', data=fileContent,
                    headers={'Hash':hash.hexdigest()},
                    timeout=20)
 
